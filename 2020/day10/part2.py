ff2ee523f1b9f5f8c5514703a907d023a57c1e34
Wedge <matthew.wedgeroberts@netacea.com>
2020-12-10 17:40:08 +0000

    day 10 solution improvement

--- 2020/day10/part2.py
+++ 2020/day10/part2.py
@@ -1,18 +1,18 @@
 def count_possibilites(joltages, last_joltage):
     if not joltages:
         return 1
+    if joltages[0] - last_joltage > 3:
+        return 0
 
     for i in range(1, len(joltages)):
         if joltages[i] - joltages[i-1] == 3:
             return count_possibilites(joltages[:i], last_joltage) * count_possibilites(joltages[i:], joltages[i-1])
 
-    return_val = 0
-    for idx, j in enumerate(joltages):
-        if j - last_joltage <= 3:
-            return_val += count_possibilites(joltages[idx + 1:], j)
-        else:
-            break
-    return return_val
+    if len(joltages) == 1:
+        return count_possibilites(joltages[1:], joltages[0])
+    else:
+        return count_possibilites(joltages[1:], joltages[0]) + count_possibilites(joltages[1:], last_joltage)
+
 
 if __name__ == "__main__":    
     joltages = [int(j) for j in open("input.txt").read().splitlines()]
