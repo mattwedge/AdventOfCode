bdfe0d46d164c3365b280ac9aeab7769f5de8c02
Wedge <matthew.wedgeroberts@gmail.com>
2021-12-01 16:41:26 +0000

    day 1 2021

---
+++ 2021/day1/both_parts.py
@@ -0,0 +1,10 @@
+def get_num_increases(arr):
+    return len([pair for pair in zip(arr, arr[1:]) if pair[1] > pair[0]])
+
+if __name__ == "__main__":
+    f = open("./input.txt", "r")
+    input_numbers = [int(num) for num in f.read().splitlines()]
+
+    print(get_num_increases(input_numbers)) 
+    print(get_num_increases([sum(tup) for tup in zip(input_numbers, input_numbers[1:], input_numbers[2:])]))
+    
