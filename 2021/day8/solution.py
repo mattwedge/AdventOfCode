8962f9bf5004a4a6138ff367ec2a4bbe8859342a
Wedge <matthew.wedgeroberts@gmail.com>
2021-12-09 00:52:26 +0000

    2021 day8 improvement

--- 2021/day8/solution.py
+++ 2021/day8/solution.py
@@ -1,6 +1,6 @@
 # 39:33
 
-from collections import defaultdict
+from collections import Counter
 
 if __name__ == "__main__":
     f = open("./input.txt", "r")
@@ -11,48 +11,41 @@ if __name__ == "__main__":
 
     # Part 2
     example_mapping = {
-        0: "cagedb",
-        1: "ab",
-        2: "gcdfa",
-        3: "fbcad",
-        4: "eafb",
-        5: "cdfbe",
-        6: "cdfgeb",
-        7: "dab",
-        8: "acedgfb",
-        9: "cefabd",
+        "cagedb": 0,
+        "ab": 1,
+        "gcdfa": 2,
+        "fbcad": 3,
+        "eafb": 4,
+        "cdfbe": 5,
+        "cdfgeb": 6,
+        "dab": 7,
+        "acedgfb": 8,
+        "cefabd": 9,
     }
 
+    def map_pattern(pattern, all_patterns):
+        """Convert the pattern into a shuffle-invariant representation.
+        
+        By counting the occurences of each letter over all patterns and replacing
+        the letter with this count, we get a representation for a pattern that will
+        be the same regardless of the shuffling. Fortunately this representation is
+        also unique so we can easily match it up with the example mappings.
+        """
+        counts = Counter("".join(all_patterns))
+        return "".join([str(a) for a in sorted([counts[letter] for letter in pattern])])
+
+    updated_example_mapping = {
+        map_pattern(k, example_mapping.keys()): v
+        for k, v in example_mapping.items()
+    }
 
     total = 0
     for line in input_lines:
         patterns = line[0]
-        output = line[1]
-        known_digits = defaultdict(str)
+        outputs = line[1]
+        updated_outputs = [map_pattern(output, patterns) for output in outputs]
+        parsed_outputs = [updated_example_mapping[output] for output in updated_outputs]
 
-        known_digits[1] = [a for a in patterns if len(a) == 2][0]
-        known_digits[4] = [a for a in patterns if len(a) == 4][0]
-        known_digits[7] = [a for a in patterns if len(a) == 3][0]
-        known_digits[8] = [a for a in patterns if len(a) == 7][0]
-
-        remaining_patterns = [a for a in patterns if not a in known_digits.values()]
-
-        for unknown_digit in set(range(10)).symmetric_difference(known_digits.keys()):
-            possible_patterns = [pattern for pattern in remaining_patterns]
-            for known_digit in known_digits:
-                expected_intersects = len(set(example_mapping[known_digit]).intersection(example_mapping[unknown_digit]))
-                possible_patterns = [
-                    pattern for pattern in possible_patterns if
-                    sum(char in pattern for char in known_digits[known_digit]) == expected_intersects
-                ]
-                
-            if len(possible_patterns) == 1:
-                known_digits[unknown_digit] = possible_patterns[0]
-            else:
-                raise Exception("Couldn't uniquely identify digit")
-
-
-        parsed_output = [[k for k, v in known_digits.items() if (len(v) == len(d) and (all([a in d for a in v])))][0] for d in output]
-        total += int("".join([str(a) for a in parsed_output]))
+        total += int("".join([str(a) for a in parsed_outputs]))
 
     print(total)
