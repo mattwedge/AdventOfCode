026e47ca383d06edaab4112ab43ffee0d99aebb3
Matt Wedge <matthew.wedgeroberts@elixirsoftware.co.uk>
2024-12-06 10:35:35 +0000

    day 4 2024 part 2

--- 2024/day4/solution.py
+++ 2024/day4/solution.py
@@ -1,5 +1,4 @@
-# 12:08
-
+# ~30:00
 import re
 import numpy as np
 
@@ -11,39 +10,38 @@ def count_horizontal(grid):
 
     return num
 
+def count_diagonal(grid):
+    num_diag = 0
+    grid_length = len(grid.diagonal())
+    for grid_init in [grid, np.fliplr(grid)]:
+        for i in range(-grid_length, grid_length):
+            diag_str = "".join(grid_init.diagonal(offset=i))
+            num_diag += len(re.findall("XMAS", diag_str))
+            num_diag += len(re.findall("SAMX", diag_str))
+
+    return num_diag
+
+def count_x(grid):
+    grid_length = len(grid.diagonal())
+    num_x = 0
+    for i in range(grid_length):
+        for j in range(grid_length):
+            subgrid = grid[i-1:i+2, j-1:j+2]
+            if "".join(subgrid.diagonal()) in ["MAS", "SAM"]:
+                if "".join(np.fliplr(subgrid).diagonal()) in ["MAS", "SAM"]:
+                    num_x += 1
+
+    return num_x
+
 if __name__ == "__main__":
     f = open("./input.txt", "r")
     input = f.read()
     lines = input.splitlines()
     grid = np.array([[char for char in line] for line in lines])
+
     num_horizontal = count_horizontal(grid)
-
     num_vertical = count_horizontal(grid.T)
+    num_diag = count_diagonal(grid)
 
-    num_diag = 0
-
-    for grid_init in [grid, np.fliplr(grid)]:
-        main_diag = grid_init.diagonal()
-        diag_str = "".join(main_diag)
-        num_diag += len(re.findall("XMAS", diag_str))
-        num_diag += len(re.findall("SAMX", diag_str))
-    
-    l = len(grid.diagonal())
-    for grid_init in [grid, np.fliplr(grid)]:
-        for i in range(1, l-1):
-            subgrid = grid_init[i:,:l-i]
-            diag_str = "".join(subgrid.diagonal())
-            num_diag += len(re.findall("XMAS", diag_str))
-            num_diag += len(re.findall("SAMX", diag_str))
-
-
-            subgrid = grid_init[:l-i,i:]
-            diag_str = "".join(subgrid.diagonal())
-            num_diag += len(re.findall("XMAS", diag_str))
-            num_diag += len(re.findall("SAMX", diag_str))
-
-
-    print(f"{num_diag = }")
-    print(f"{num_horizontal = }")
-    print(f"{num_vertical = }")
     print(num_diag + num_horizontal + num_vertical)
+    print(count_x(grid))
