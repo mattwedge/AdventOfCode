2c4a3e25f58e1c7375bb4f45f9f41568afdc70d3
Matt Wedge <matthew.wedgeroberts@elixirsoftware.co.uk>
2024-12-10 14:47:24 +0000

    day 9 2024 tidy up

--- 2024/day9/solution.py
+++ 2024/day9/solution.py
@@ -1,15 +1,15 @@
 # 57:20
 
-f = open("./input.txt", "r")
-input = f.read()
-
-def expand_digits(arr: list[str]) -> list[int, str]:
+def expand_files(file_clusters: list[list[int]], empty_blocks: list[int], file_sizes: list[int]):
     expanded = []
-    for i, char in enumerate(arr):
-        if not i % 2:
-            expanded += [int(i / 2) for _ in list(range(int(char)))]
-        else:
-            expanded += ["." for _ in list(range(int(char)))]
+    for cluster_number, cluster in enumerate(file_clusters):
+        # Place each file in the cluster into the array
+        for file_id in cluster:
+            file_size = file_sizes[file_id]
+            expanded += [file_id for _ in range(file_size)]
+
+        # Add the whitespace
+        expanded += ["." for _ in range(empty_blocks[cluster_number])]
 
     return expanded
 
@@ -20,69 +20,55 @@ def sum_from_position(arr: list[int, str]) -> int:
             total += i * int(num)
     return total
 
-def part_1(arr: list[int, str]):
-    for i in range(len(arr)):
-        num = arr[-1 - i]
-        first_null = arr.index(".")
-        if first_null > len(arr) - 1 - i:
+def part_1(arr: list[int]):
+    file_sizes = arr[::2]
+    empty_block_sizes = [int(a) for a in arr[1::2]] + [0]
+    expanded_arr = expand_files(
+        [[a] for a in list(range(len(file_sizes)))],
+        empty_block_sizes,
+        file_sizes
+    )
+    for i in range(len(expanded_arr)):
+        num = expanded_arr[-1 - i]
+        first_null = expanded_arr.index(".")
+        if first_null > len(expanded_arr) - 1 - i:
             break
 
-        arr[first_null] = num
-        arr[len(arr) - i - 1] = "."
+        expanded_arr[first_null] = num
+        expanded_arr[len(expanded_arr) - i - 1] = "."
 
-    return sum_from_position(arr)
+    return sum_from_position(expanded_arr)
 
 def part_2(arr: list[int, str]):
-    processed_nums = set() # Keep a record of which blocks we've processed
-    for file_position in range(len(arr)):
-        num = arr[-1-file_position]
-        if num == ".":
-            continue
+    file_sizes = arr[::2]
+    empty_blocks = arr[1::2] + [0]
 
-        if num in processed_nums:
-            continue
-
-        if num == 0:
-            break
-
-        processed_nums.add(num)
-
-        file_length = 1
-        while arr[-1-file_position-file_length] == num:
-            file_length += 1
-            if 1 + file_position + file_length >= len(arr):
+    file_clusters = [[file_id] for file_id in range(len(file_sizes))]
+    for file_cluster in file_clusters[::-1]:
+        file_id = file_cluster[0]
+        file_size = file_sizes[file_id]
+        for block_size in empty_blocks[:file_id]:
+            if block_size >= file_size:
+                block_number = empty_blocks.index(block_size)
+                file_clusters[block_number].append(file_id)
+                empty_blocks[block_number] -= file_size
+                empty_blocks[file_id - 1] += file_size
+                file_cluster.pop(0) # Remove from the original cluster 
                 break
 
+    
+    # Now file_clusters is a list of lists of file IDs, and empty_blocks defines
+    # the empty space in between each cluster of file IDs. We just need to
+    # reconstruct the expanded representation and calculate the sum
 
-        for starting_position, file_id in enumerate(arr):
-            if starting_position >= len(arr) - file_position:
-                break
-
-            if file_id == ".":
-                blocked = any(
-                    (
-                        starting_position + l >= len(arr)
-                        or arr[starting_position + l] != "."
-                    )
-                    for l in range(file_length)
-                )
-
-                if blocked:
-                    continue
-
-                # Valid placement - move block
-                for l in range(file_length):
-                    arr[starting_position + l] = num
-                    arr[-1 - file_position - l] = "."
-                
-                break
+    return sum_from_position(expand_files(file_clusters, empty_blocks, file_sizes))
 
 
-    return sum_from_position(arr)
+f = open("./input.txt", "r")
+input = [int(num) for num in f.read()]
 
-        
-part_1_total = part_1(expand_digits(list(input)))
+part_1_total = part_1(input)
 print(part_1_total)
 
-part_2_total = part_2(expand_digits(list(input)))
+part_2_total = part_2(input)
 print(part_2_total)
