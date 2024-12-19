113730e0dbd87578f20d6a626465c5c35e47b4c3
Matt Wedge <matthew.wedgeroberts@elixirsoftware.co.uk>
2024-12-19 13:42:57 +0000

    day 19 2024 remove lines

--- 2024/day19/solution.py
+++ 2024/day19/solution.py
@@ -2,16 +2,12 @@
 
 from functools import lru_cache
 
-f = open("./input.txt", "r")
-lines = f.read().splitlines()
-pats = lines[0].split(", ")
-designs = lines[2:]
+lines = open("./input.txt", "r").read().splitlines()
+pats, designs = lines[0].split(", "), lines[2:]
 
 @lru_cache
 def get_num_sols(design):
-    if design == "":
-        return 1
-    return sum(get_num_sols(design[len(pattern):]) for pattern in pats if design.startswith(pattern))
+    return 1 if not design else sum(get_num_sols(design[len(pattern):]) for pattern in pats if design.startswith(pattern))
 
 sols = [get_num_sols(d) for d in designs]
 print(len([s for s in sols if s]))
