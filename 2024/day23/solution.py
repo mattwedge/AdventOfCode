dba304ea32bca660b2aa1a11a36a0d81aceaedc7
Matt Wedge <matthew.wedgeroberts@elixirsoftware.co.uk>
2024-12-24 13:41:38 +0000

    day 23 2024

--- 2024/day23/solution.py
+++ 2024/day23/solution.py
@@ -1,24 +1,18 @@
-import functools
 from itertools import combinations
-# import networkx as nx
+import networkx as nx
 
 lines = [set(a.split("-")) for a in open("./input.txt", "r").read().splitlines()]
-nodes = functools.reduce(lambda a, b: a | b, lines, set())
-print(nodes)
-print(lines)
-total = 0
-trips = []
-nodes_list = list(nodes)
-for asd, node in enumerate(nodes_list):
-    print(asd)
-    matching_nodes = [n for n in nodes_list[asd:] if {n, node} in lines]
-    for matching_node in matching_nodes:
-        qwe = nodes_list.index(matching_node)
-        final_nodes = [n for n in nodes_list[qwe:] if {n, matching_node} in lines and n != node]
-        for final_node in final_nodes:
-            if {final_node, node} in lines:
-                total += 1
+g = nx.Graph(lines)
 
-print(total)
+cliques = list(nx.find_cliques(g))
+maxclique = max(len(c) for c in cliques)
+threecliques = set()
+for c in cliques:
+    if len(c) == maxclique:
+        print("Part 2: " + ','.join(sorted(c)))
+    for a, b, c in combinations(c, 3):
+        if a.startswith("t") or b.startswith("t") or c.startswith("t"):
+            threecliques.add(",".join(sorted([a, b, c])))
 
-# 11011 too high
+
+print("Part 1: " + str(len(threecliques)))
